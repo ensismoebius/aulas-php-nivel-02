b84eb53902911a008721fb14cb99b0fdde5d4c82
André Furlan <ensismoebius@gmail.com>
2018-09-03 21:10:47 -0300

    Atualizações

--- quitanda/controller/Loja.php
+++ quitanda/controller/Loja.php
@@ -7,9 +7,30 @@ require_once 'lib/BancoDeDados.php';
 use model;
 
 class Loja {
+
+	/**
+	 * Verifica se o cliente existe no banco de dados
+	 * caso ele exista retorna seu código senão
+	 * retorna -1
+	 * @param model\Cliente $c
+	 * @return number
+	 */
 	public function autenticarCliente(model\Cliente $c) {
-		$bd = new \BancoDeDados();
-		
-		
+		$bd = new \BancoDeDados ();
+
+		if ($bd->abrirConexao ()) {
+
+			$sql = "Select cod from Cliente where login = '{$c->getLogin()}' and senha='{$c->getSenha()}'";
+
+			$bd->executarSQL ( $sql );
+
+			$resultado = $bd->lerResultados ();
+
+			if (count ( $resultado ) > 0) {
+				return $resultado [0] ["cod"];
+			}
+
+			return - 1;
+		}
 	}
 }
--- quitanda/model/Cliente.php
+++ quitanda/model/Cliente.php
@@ -1,246 +1,218 @@
 <?php
+
 namespace model;
-class Cliente
-{
-    private $cod;
 
-    private $nome;
+class Cliente {
+	private $cod;
+	private $login;
+	private $senha;
+	private $nome;
+	private $email;
+	private $telefone;
+	private $cpf;
+	private $logradouro;
+	private $numeroDaCasa;
+	private $cep;
+	private $complemento;
+	private $cidade;
+	private $estado;
+	private $pais;
 
-    private $email;
+	/**
+	 * @return mixed
+	 */
+	public function getLogin() {
+		return $this->login;
+	}
 
-    private $telefone;
+	/**
+	 * @return mixed
+	 */
+	public function getSenha() {
+		return $this->senha;
+	}
 
-    private $cpf;
+	/**
+	 * @param mixed $login
+	 */
+	public function setLogin($login) {
+		$this->login = $login;
+	}
 
-    private $logradouro;
+	/**
+	 * @param mixed $senha
+	 */
+	public function setSenha($senha) {
+		$this->senha = $senha;
+	}
 
-    private $numeroDaCasa;
+	/**
+	 * @return mixed
+	 */
+	public function getCod() {
+		return $this->cod;
+	}
 
-    private $cep;
+	/**
+	 * @return mixed
+	 */
+	public function getNome() {
+		return $this->nome;
+	}
 
-    private $complemento;
+	/**
+	 * @return mixed
+	 */
+	public function getEmail() {
+		return $this->email;
+	}
 
-    private $cidade;
+	/**
+	 * @return mixed
+	 */
+	public function getTelefone() {
+		return $this->telefone;
+	}
 
-    private $estado;
+	/**
+	 * @return mixed
+	 */
+	public function getCpf() {
+		return $this->cpf;
+	}
 
-    private $pais;
+	/**
+	 * @return mixed
+	 */
+	public function getLogradouro() {
+		return $this->logradouro;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getCod()
-    {
-        return $this->cod;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getNumeroDaCasa() {
+		return $this->numeroDaCasa;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getNome()
-    {
-        return $this->nome;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getCep() {
+		return $this->cep;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getComplemento() {
+		return $this->complemento;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getTelefone()
-    {
-        return $this->telefone;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getCidade() {
+		return $this->cidade;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getCpf()
-    {
-        return $this->cpf;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getEstado() {
+		return $this->estado;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getLogradouro()
-    {
-        return $this->logradouro;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getPais() {
+		return $this->pais;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getNumeroDaCasa()
-    {
-        return $this->numeroDaCasa;
-    }
+	/**
+	 * @param mixed $cod
+	 */
+	public function setCod($cod) {
+		$this->cod = $cod;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getCep()
-    {
-        return $this->cep;
-    }
+	/**
+	 * @param mixed $nome
+	 */
+	public function setNome($nome) {
+		$this->nome = $nome;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getComplemento()
-    {
-        return $this->complemento;
-    }
+	/**
+	 * @param mixed $email
+	 */
+	public function setEmail($email) {
+		$this->email = $email;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getCidade()
-    {
-        return $this->cidade;
-    }
+	/**
+	 * @param mixed $telefone
+	 */
+	public function setTelefone($telefone) {
+		$this->telefone = $telefone;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getEstado()
-    {
-        return $this->estado;
-    }
+	/**
+	 * @param mixed $cpf
+	 */
+	public function setCpf($cpf) {
+		$this->cpf = $cpf;
+	}
 
-    /**
-     *
-     * @return mixed
-     */
-    public function getPais()
-    {
-        return $this->pais;
-    }
+	/**
+	 * @param mixed $logradouro
+	 */
+	public function setLogradouro($logradouro) {
+		$this->logradouro = $logradouro;
+	}
 
-    /**
-     *
-     * @param mixed $cod
-     */
-    public function setCod($cod)
-    {
-        $this->cod = $cod;
-    }
+	/**
+	 * @param mixed $numeroDaCasa
+	 */
+	public function setNumeroDaCasa($numeroDaCasa) {
+		$this->numeroDaCasa = $numeroDaCasa;
+	}
 
-    /**
-     *
-     * @param mixed $nome
-     */
-    public function setNome($nome)
-    {
-        $this->nome = $nome;
-    }
+	/**
+	 * @param mixed $cep
+	 */
+	public function setCep($cep) {
+		$this->cep = $cep;
+	}
 
-    /**
-     *
-     * @param mixed $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
+	/**
+	 * @param mixed $complemento
+	 */
+	public function setComplemento($complemento) {
+		$this->complemento = $complemento;
+	}
 
-    /**
-     *
-     * @param mixed $telefone
-     */
-    public function setTelefone($telefone)
-    {
-        $this->telefone = $telefone;
-    }
+	/**
+	 * @param mixed $cidade
+	 */
+	public function setCidade($cidade) {
+		$this->cidade = $cidade;
+	}
 
-    /**
-     *
-     * @param mixed $cpf
-     */
-    public function setCpf($cpf)
-    {
-        $this->cpf = $cpf;
-    }
+	/**
+	 * @param mixed $estado
+	 */
+	public function setEstado($estado) {
+		$this->estado = $estado;
+	}
 
-    /**
-     *
-     * @param mixed $logradouro
-     */
-    public function setLogradouro($logradouro)
-    {
-        $this->logradouro = $logradouro;
-    }
-
-    /**
-     *
-     * @param mixed $numeroDaCasa
-     */
-    public function setNumeroDaCasa($numeroDaCasa)
-    {
-        $this->numeroDaCasa = $numeroDaCasa;
-    }
-
-    /**
-     *
-     * @param mixed $cep
-     */
-    public function setCep($cep)
-    {
-        $this->cep = $cep;
-    }
-
-    /**
-     *
-     * @param mixed $complemento
-     */
-    public function setComplemento($complemento)
-    {
-        $this->complemento = $complemento;
-    }
-
-    /**
-     *
-     * @param mixed $cidade
-     */
-    public function setCidade($cidade)
-    {
-        $this->cidade = $cidade;
-    }
-
-    /**
-     *
-     * @param mixed $estado
-     */
-    public function setEstado($estado)
-    {
-        $this->estado = $estado;
-    }
-
-    /**
-     *
-     * @param mixed $pais
-     */
-    public function setPais($pais)
-    {
-        $this->pais = $pais;
-    }
+	/**
+	 * @param mixed $pais
+	 */
+	public function setPais($pais) {
+		$this->pais = $pais;
+	}
 }
 
 
