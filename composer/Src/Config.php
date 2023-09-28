557ef3a7f8e6366e81a2ddcb7cbecefb2c2ef75c
Andre Furlan <ensismoebius@gmail.com>
2023-09-27 21:32:21 -0300

    Fix

--- composer/Src/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-define("URL", "http://localhost/aulas-php-nivel-02/composer");
-
-define("DB_SENHA", "1234");
-define("DB_LOGIN", "root");
-define("DB_URL", "mysql:host=127.0.0.1;dbname=teste");
