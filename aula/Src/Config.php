6a2fd88514d1e2d4eea1e5c877b8ccbe9a1cf5a0
Andre Furlan <ensismoebius@gmail.com>
2023-10-18 21:09:02 -0300

    dd

--- aula/Src/Config.php
+++ aula/Src/Config.php
@@ -10,5 +10,5 @@ define("IMAGE_DIR", "./Src/View/Images");
 
 // Configurações do banco de dados
 define("BD_ENDERECO", "mysql:host=127.0.0.1;dbname=mercadinho");
-define("BD_USUARIE", "andre");
+define("BD_USUARIE", "root");
 define("BD_SENHA", "1234");
